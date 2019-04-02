4ec1192fb611beb78f6a0dd721e802e5f3966c4d
David Mathis <dbmathis@gmail.com>
2019-04-01 21:02:01 -0400

    added random key/value gen to fill-memory.sh script

--- fill-memory.sh
+++ fill-memory.sh
@@ -2,3 +2,8 @@
 # Use example app restful API to test different scenarios with redis instances
 
 export APP=redis-example-app.cfapps-17.haas-59.pez.pivotal.io
+
+key=$(LC_CTYPE=C tr -dc A-Za-z0-9 < /dev/urandom | head -c 12 | xargs)
+value=$(LC_CTYPE=C tr -dc A-Za-z0-9 < /dev/urandom | head -c 32 | xargs)
+
+curl -X PUT "${APP}/${key}" -d "data=${value}"
